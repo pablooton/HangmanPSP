14bdd86e5c4afa63ae2993cb002431e6c0e3d5d0
pablo <pablootoncaridadzalaeta@gmail.com>
2026-01-29 10:36:14 +0100

    Creación Login

---
+++ Desktop/HangmanPSP/src/main/java/edu/pmoc/practicatrim/hangmanpsp/LoginController.java
@@ -0,0 +1,9 @@
+package edu.pmoc.practicatrim.hangmanpsp;
+
+import javafx.event.ActionEvent;
+
+public class LoginController {
+
+    public void onPlayButtonClick(ActionEvent actionEvent) {
+    }
+}
