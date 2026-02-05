5c49371ce143234981744d925734e8aeedbeb597
Manuel García <manuel.garcia.rey.48@gmail.com>
2026-02-05 10:23:44 +0100

    LifeCounter Adding and GameController lifes implementation

--- Desktop/HangmanPSP/src/main/java/edu/pmoc/practicatrim/hangmanpsp/MainApp.java
+++ Desktop/HangmanPSP/src/main/java/edu/pmoc/practicatrim/hangmanpsp/MainApp.java
@@ -11,7 +11,7 @@ public class MainApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("/fxml/login-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 500, 340);
+        Scene scene = new Scene(fxmlLoader.load(), 600, 420);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
--- Desktop/HangmanPSP/src/main/java/edu/pmoc/practicatrim/hangmanpsp/controller/GameController.java
+++ Desktop/HangmanPSP/src/main/java/edu/pmoc/practicatrim/hangmanpsp/controller/GameController.java
@@ -1,28 +1,51 @@
 package edu.pmoc.practicatrim.hangmanpsp.controller;
-import javafx.event.ActionEvent;
+
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.layout.TilePane;
+import javafx.event.ActionEvent;
 
 public class GameController {
-
 
     @FXML
     private Label lblPalabra;
 
     @FXML
+    private TilePane panelLetras;
+    @FXML
+    private Label lblVidas;
+
+    private int vidasRestantes = 6;
+
+    @FXML
     public void initialize() {
-        lblPalabra.setText("_ _ _ A _ _");
+        vidasRestantes = 6;
+        lblVidas.setText(String.valueOf(vidasRestantes));
     }
 
     @FXML
     public void pulsarLetra(ActionEvent event) {
-        Button botonPulsado = (Button) event.getSource();
-        String letra = botonPulsado.getText();
+        Button boton = (Button) event.getSource();
+        String letra = boton.getText();
 
-        System.out.println("Letra enviada: " + letra);
+        boton.setDisable(true);
+        boolean acierto = false;
 
-        botonPulsado.setDisable(true);
+        if (!acierto) {
+            restarVida();
+        }
+    }
 
+    private void restarVida() {
+        if (vidasRestantes > 0) {
+            vidasRestantes--;
+            lblVidas.setText(String.valueOf(vidasRestantes));
+
+            if (vidasRestantes == 0) {
+                System.out.println("GAME OVER - Has perdido");
+                panelLetras.setDisable(true);
+            }
+        }
     }
 }
